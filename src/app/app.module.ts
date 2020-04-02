a77ee2e3c9b2e849074bb44d8194f2cdf83a8bdb
Giovanni <giovanni.dallariva@lynxspa.com>
2020-04-02 17:57:26 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,29 @@ import { RegioniProvinceComponent } from './components/regioni-province/regioni-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { NationalTrend } from './components/national-trend-chart/national-trend-chart.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTableModule} from '@angular/material/table';
+import {MatListModule} from '@angular/material/list';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {FormsModule} from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs'
+import { SummaryComponent } from './summary/summary.component';
+import { GraficiComponent} from "./components/grafici/grafici.component";
+import { NotesComponent} from "./components/notes/notes.component";
+import { TotaliPositiviComponent} from "./components/grafici/variazioni/totali-positivi.component";
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NationalTrend,
     AppComponent,
-    RegioniProvinceComponent
+    RegioniProvinceComponent,
+    SummaryComponent,
+    GraficiComponent,
+    NotesComponent,
+    TotaliPositiviComponent
+
   ],
   imports: [
     BrowserModule,
@@ -24,6 +40,15 @@ import { NationalTrend } from './components/national-trend-chart/national-trend-
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatListModule,
+    MatExpansionModule,
+    FormsModule,
+    MatTabsModule,
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
